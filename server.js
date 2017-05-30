a713f35e56c142dcff50d67ce7f135e30373a546
lupin <mail@lp1.eu>
2017-05-30 23:01:23 +0200

    updated syntaxes and added telegram bot

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 #!/usr/bin/node
 
 (function (){
+    //parameters validation
+    const usage         = function(){
+        console.error('usage : ' + process.argv[0] + ' [deepstream_app_url] [telegram_token] (port)')
+        return 1
+    }    
+    if (process.argv.length < 4){
+        return usage()
+    }
+    
     //constants
     const version       = 0.01
     const queue_size    = 10;
     const deepstream    = require('deepstream.io-client-js')
-    const express       = require('express')
-    const usage         = function(){
-        console.error('usage : ' + process.argv[0] + ' [appUrl] (port)')
-        return 1
-    }    
-    if (process.argv.length < 3){
-        return usage()
-    }
     const ds            = deepstream(process.argv[2])
     
     //deepstreamhub
     ds.login()
     var messageQueue    = []
-    var record          = ds.record.getRecord('test')
+    var record          = ds.record.getRecord('notificationhub')
     emitInterval        = setInterval(function() {
         record.set({
             messageQueue: messageQueue
@@ -26,9 +27,10 @@
     }, 1000)
     
     //express
+    const express	= require('express')
     var app         = express()
-    var port        = process.argv[3]
-    
+    var port		= process.argv[4]
+
     if (isNaN(parseFloat(port)) || !isFinite(port)){
         port = 3000;
     }
@@ -50,5 +52,27 @@
     app.listen(port, function () {
         console.log('Notifications Hub listening on port ' + port)
     })
+    
+    //telegram bot
+    const TelegramBot = require('node-telegram-bot-api')
+    const token = process.argv[3]
+    const bot = new TelegramBot(token, {polling: true});    
+
+    bot.onText(/help (.+)/, (msg, match) => {
+	    console.log('help')
+	    const chatId = msg.chat.id;
+	    const rest = "I cannot help you";
+	    bot.sendMessage(chatId, rest);
+    });
+    
+    bot.onText(/\/echo (.+)/, (msg, match) => {
+	    const chatId = msg.chat.id;
+	    const resp = match[1];
+	    bot.sendMessage(chatId, resp);
+    });
+
+    bot.on('message', (msg) => {
+	    const chatId = msg.chat.id;
+    });
     
 })();
